a054b4d29753450432f0c704d2695e2c293f8424
Abdullah-Hajjar <abdullahhajjar12@gmail.com>
2024-11-23 15:31:32 +0300

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,1 @@
+window.$crisp=[];window.CRISP_WEBSITE_ID="3e9fcfba-cf30-4135-a1e5-9a2e1d9c806b";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
